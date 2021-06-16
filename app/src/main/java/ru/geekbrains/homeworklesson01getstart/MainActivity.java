8964fd454a810dbe9f009aa19aa7376a7b69ecf3
kalinovdv <kdv_ul@rambler.ru>
2021-06-16 16:38:23 +0400

    Отредактировал процедуру onCreate для запуска первого макета.

--- app/src/main/java/ru/geekbrains/homeworklesson01getstart/MainActivity.java
+++ app/src/main/java/ru/geekbrains/homeworklesson01getstart/MainActivity.java
@@ -9,8 +9,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
 //        setContentView(R.layout.activity_main2);
-        setContentView(R.layout.activity_main3);
+//        setContentView(R.layout.activity_main3);
     }
 }
